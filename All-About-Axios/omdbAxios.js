74b1bcfdb88a0742320d55380128f281feaf0655
Tamar <mischegoss@gmail.com>
2020-02-29 01:05:41 -0500

    Fix API

--- All-About-Axios/omdbAxios.js
+++ All-About-Axios/omdbAxios.js
@@ -2,7 +2,7 @@
 // Here we incorporate the "axios" npm package
 var axios = require("axios");
 
-//Check out the Inquirer page to figure out how this works.
+//Check out the Inquirer tutorial page to figure out how this works.
 
 // We then run the request with axios module on a URL with a JSON
 axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
@@ -12,10 +12,10 @@ axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=tr
     console.log("The movie's rating is: " + response.data.imdbRating);
   }
 );
-/* Okay, this is not that different from the ajax call. YOu call axios and then get 
-   (remeber GET is a method to retreive data). It is going to take in the url.
+/* Okay, this is not that different from the ajax call. You call axios and then get 
+   (remember GET is a method to retreive data). It is going to take in the url.
 
    THEN you are going to get a response. It looks like the response contains something called data
-   and in that data one layer down is the imdbRating.
+   and in that data one layer down is the imdbRating. Want to see it? Console log it.
 
    */
